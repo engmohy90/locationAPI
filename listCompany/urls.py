983c6d5b4db84b9a7d792ea2e414ee281d806a78
mohamed.mohy <mohammad.mohy@businessclouds.biz>
2018-07-08 17:16:50 +0200

    add static

--- listCompany/urls.py
+++ listCompany/urls.py
@@ -19,4 +19,4 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('API.urls'))
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
